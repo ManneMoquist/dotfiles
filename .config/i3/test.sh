6f701a81b80995e9d02cf152b6eada3bc700f3a8
Manne Moquist <mannemoquist@gmail.com>
2018-01-07 18:04:08 +0100

    added keyboard layout to the i3bar

---
+++ .config/i3/test.sh
@@ -0,0 +1,9 @@
+cd /home/mannemoquist/.config/i3/
+i3status | ( read line && echo $line && read line && echo $line && while :
+do
+	read line
+	language=$(setxkbmap -query | grep layout)
+	keyboard_json=",{\"name\":\"keyboard\",\"full_text\":\"${language}\" }]"
+	echo "${line%?}${keyboard_json}" 
+done)
+
