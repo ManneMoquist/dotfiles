c1a23bb577b352d524f9db548a73b93e9492c8e1
Manne Moquist <mannemoquist@gmail.com>
2018-01-20 13:05:43 +0100

    added the jj combination for exiting insert mode in vim

--- .config/i3/test.sh
+++ .config/i3/test.sh
@@ -3,7 +3,7 @@ i3status | ( read line && echo $line && read line && echo $line && while :
 do
 	read line
 	language=$(setxkbmap -query | grep layout)
-	keyboard_json=",{\"name\":\"keyboard\",\"full_text\":\"${language}\" }]"
-	echo "${line%?}${keyboard_json}" 
+	keyboard_json=",{\"name\":\"keyboard\",\"full_text\":\"${language}\" }"
+	echo "${line%?}${keyboard_json}]" 
 done)
 
